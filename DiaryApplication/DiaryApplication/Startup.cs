35806264bd8b47ed7899eaf53cc0b79051f1222b
marwamoronya <amosmarwa@dynasoft.co.ke>
2023-01-26 03:25:25 +0300

    Generated a new project

---
+++ DiaryApplication/DiaryApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DiaryApplication.Startup))]
+namespace DiaryApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
